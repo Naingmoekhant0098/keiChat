7931c71e94163a5f0d4536fc23843b7668db2fb1
Kei <keikazuki0098@gmail.com>
2024-05-25 10:56:17 -0700

    ft

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="text-center">
             <h3 className="text-2xl mb-3"> Download Our <span className=' text-yellow-400'>Chat</span> app  </h3>
             <p> Download the app now and embrace the power of 'then' to make every moment count. </p>
-            <div className="flex justify-center my-10">
+            <div className="flex justify-center my-10 px-4">
                 <div className="flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2">
                     <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" className="w-7 md:w-8" />
                     <div className="text-left ml-3">
