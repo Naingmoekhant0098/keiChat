79fefe9a68a5290bfd541886b515b7f0a345bcd8
Kei <keikazuki0098@gmail.com>
2024-06-11 05:39:54 -0700

    repair

--- client/src/Components/Message.jsx
+++ client/src/Components/Message.jsx
@@ -55,7 +55,7 @@ const Message = ({ data, handleDelete, currentchat, sentReaction, index }) => {
                     return (
                       <span
                         key={ie}
-                        className={`   px-1 rounded-full text-xl ${
+                        className={`   px-1 rounded-full text-lg ${
                           like.senderId === currentUser._id
                             ? "bg-cyan-500"
                             : "bg-slate-400 disabled"
